e398db2e7fbc6b0accbfc279c55dd3374b08c9e4
Manolo Cantón <manuelcanton8@gmail.com>
2024-07-05 01:27:31 +0200

    Removing plugin Project Settings
    There are bugs with project settings.

--- addons/scene_manager/plugin.gd
+++ addons/scene_manager/plugin.gd
@@ -9,19 +9,6 @@ func _enter_tree() -> void:
 	# Removes files from test and editor folders.
 	_export_plugin = preload("res://addons/scene_manager/editor/export.gd").new()
 	add_export_plugin(_export_plugin)
-	
-	#region Settings
-	if not ProjectSettings.has_setting(LoadingScreen.SETTING_NAME):
-		ProjectSettings.set_setting(LoadingScreen.SETTING_NAME, LoadingScreen.SETTING_DEFAULT_VALUE)
-	ProjectSettings.set_initial_value(LoadingScreen.SETTING_NAME, LoadingScreen.SETTING_DEFAULT_VALUE)
-	ProjectSettings.set_as_basic(LoadingScreen.SETTING_NAME, true)
-	ProjectSettings.add_property_info({
-		name = LoadingScreen.SETTING_NAME,
-		type = TYPE_STRING,
-		hint = PROPERTY_HINT_FILE,
-		hint_string = "*.scn,*.tscn",
-	})
-	#endregion
 
 
 func _exit_tree() -> void:
